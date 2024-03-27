e9e87920631ae338d63c8d7dae0891ab7d98e481
Cleiton Viana <cleitonviana.2@outlook.com.br>
2024-03-27 16:38:20 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cleitonSilvaViana/social-go
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,12 @@
+/*
+	Author: Cleiton Viana
+*/
+
+package main 
+
+import "fmt"
+
+
+func main() {
+	fmt.Println("HIIII")
+}
